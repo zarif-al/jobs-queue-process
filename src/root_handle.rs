fec4ab1a78e70598e422441b718385fa90015c9d
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-10-15 20:14:09 +0600

    feat: send job through transmitter

--- src/root_handle.rs
+++ src/root_handle.rs
@@ -1,12 +1,17 @@
 use axum::{http::StatusCode, Json};
 use serde::Serialize;
+use tokio::sync::mpsc::Sender;
 
 #[derive(Serialize)]
 pub struct Response {
     message: String,
 }
 
-pub async fn handle() -> (StatusCode, Json<Response>) {
+pub async fn handle(tx: Sender<String>) -> (StatusCode, Json<Response>) {
+    tx.send(String::from("job"))
+        .await
+        .expect("Expected: Send job down the channel.");
+
     (
         StatusCode::OK,
         Json(Response {
