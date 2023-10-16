fa41802d6b846e1c8daa5f1bde9c3e73e094eba4
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-10-16 22:28:56 +0600

    refactor: use `dotenv` and attempt to reuse connections

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+extern crate dotenv;
+
 mod connection_check;
 mod root_route;
 
 use axum::{routing::post, Router};
+use dotenv::dotenv;
+use redis_work_queue::{KeyPrefix, WorkQueue};
 use serde::Serialize;
+use std::env;
 use tokio::sync::mpsc;
 #[derive(Serialize)]
 pub struct Response {
@@ -11,43 +16,65 @@ pub struct Response {
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() {
-    let redis_connected = connection_check::redis_conn_check().await;
+    // Load env form .env
+    dotenv().ok();
 
-    println!("DB Connected: {redis_connected}");
+    // Check env
+    let redis_url = env::var("REDIS_URL").expect("REDIS_URL is not set in .env");
 
-    if redis_connected {
-        // transmitters and receivers
-        let (tx, rx) = mpsc::channel::<String>(32);
+    // Check connections
+    let redis_connected = connection_check::redis_conn_check(&redis_url).await;
 
-        // build our application with a single route
-        let app = Router::new().route(
-            "/",
-            post({
-                let tx_clone = tx.clone();
-                move || root_route::handle(tx_clone)
-            }),
-        );
-
-        // thread to listen and add jobs to queue
-        tokio::spawn(root_route::queue_thread(String::from("Route Thread 1"), rx));
-
-        // threads to process jobs from queue
-        tokio::spawn(root_route::processing_thread(String::from(
-            "Processing Thread 1",
-        )));
-        tokio::spawn(root_route::processing_thread(String::from(
-            "Processing Thread 2",
-        )));
-        tokio::spawn(root_route::processing_thread(String::from(
-            "Processing Thread 3",
-        )));
-
-        // run it with hyper on localhost:4000
-        axum::Server::bind(&"0.0.0.0:4000".parse().unwrap())
-            .serve(app.into_make_service())
-            .await
-            .unwrap();
+    let redis_conn;
+    // let redis_ref;
+    match redis_connected {
+        Some(conn) => {
+            redis_conn = conn;
+            // redis_ref = Arc::new(Mutex::new(conn));
+            println!("DB Connected!");
+        }
+        None => panic!("Could not establish connection to db."),
     }
 
-    panic!("Could not establish connection to db.");
+    let work_queue = WorkQueue::new(KeyPrefix::from("sanity_custom_sync_rust"));
+
+    // transmitters and receivers
+    let (tx, rx) = mpsc::channel::<String>(32);
+
+    // build our application with a single route
+    let app = Router::new().route(
+        "/",
+        post({
+            let tx_clone = tx.clone();
+            move || root_route::handle(tx_clone)
+        }),
+    );
+
+    // thread to listen and add jobs to queue
+    tokio::spawn(root_route::queue_thread(
+        String::from("Route Thread 1"),
+        rx,
+        redis_conn,
+        work_queue,
+    ));
+
+    // threads to process jobs from queue
+    // tokio::spawn(root_route::processing_thread(
+    //     "Processing Thread 1".to_string(),
+    //     redis_conn,
+    // ));
+    // tokio::spawn(root_route::processing_thread(
+    //     "Processing Thread 2".to_string(),
+    //     redis_conn,
+    // ));
+    // tokio::spawn(root_route::processing_thread(
+    //     "Processing Thread 3".to_string(),
+    //     redis_conn,
+    // ));
+
+    // run it with hyper on localhost:4000
+    axum::Server::bind(&"0.0.0.0:4000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
