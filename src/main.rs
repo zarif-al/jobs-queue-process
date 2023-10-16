c56a4148364a27653ecfef6a5d6a18aeaf8f5735
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-10-16 11:22:08 +0600

    refactor: call thread fn from `root_route`

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-mod root_handle;
-
-use std::time::Duration;
+mod root_route;
 
 use axum::{routing::post, Router};
-use redis_work_queue::{Item, KeyPrefix, WorkQueue};
 use serde::Serialize;
-use tokio::{sync::mpsc, time::sleep};
+use tokio::sync::mpsc;
 #[derive(Serialize)]
 pub struct Response {
     message: String,
@@ -13,188 +10,31 @@ pub struct Response {
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() {
-    const PROCESSING_TIME: Duration = Duration::from_secs(30 * 10);
     // transmitters and receivers
-    let (tx, mut rx) = mpsc::channel::<String>(32);
+    let (tx, rx) = mpsc::channel::<String>(32);
 
     // build our application with a single route
     let app = Router::new().route(
         "/",
         post({
             let tx_clone = tx.clone();
-            move || root_handle::handle(tx_clone)
+            move || root_route::handle(tx_clone)
         }),
     );
 
     // thread to listen and add jobs to queue
-    tokio::spawn(async move {
-        // connect to redis
-        let host = "localhost";
-        let db = &mut redis::Client::open(format!("redis://{host}/"))
-            .expect("Expected: Rust db")
-            .get_async_connection()
-            .await
-            .expect("Expected: Async connection to Rust db");
+    tokio::spawn(root_route::queue_thread(String::from("Route Thread 1"), rx));
 
-        let work_queue = WorkQueue::new(KeyPrefix::from("sanity_custom_sync_rust"));
-
-        loop {
-            for received in rx.recv().await.iter() {
-                let job = Item::from_string_data(String::from(received));
-
-                work_queue
-                    .add_item(db, &job)
-                    .await
-                    .expect("Expected: Successfull addition of job to queue");
-
-                println!("Route Thread => Added job to queue. Job ID: {}", job.id);
-            }
-        }
-    });
-
-    // thread to process jobs from queue
-    tokio::spawn(async {
-        // connect to redis
-        let host = "localhost";
-        let db = &mut redis::Client::open(format!("redis://{host}/"))
-            .expect("Expected: Rust db")
-            .get_async_connection()
-            .await
-            .expect("Expected: Async connection to Rust db");
-
-        let work_queue = WorkQueue::new(KeyPrefix::from("sanity_custom_sync_rust"));
-
-        loop {
-            let job: Option<Item> = work_queue
-                .lease(db, None, Duration::from_secs(60))
-                .await
-                .expect("Expected: Lease a job");
-
-            match job {
-                Some(job) => {
-                    println!("Processing Thread 1 => Processing Job: {}", job.id);
-                    sleep(PROCESSING_TIME).await;
-                    work_queue
-                        .complete(db, &job)
-                        .await
-                        .expect("Expected: Mark job as complete from: processing thread 1");
-                    println!(
-                        "Processing Thread 1 => Completed Processing Job: {}",
-                        job.id
-                    );
-                }
-                None => {}
-            }
-        }
-    });
-
-    // thread to process jobs from queue
-    tokio::spawn(async {
-        // connect to redis
-        let host = "localhost";
-        let db = &mut redis::Client::open(format!("redis://{host}/"))
-            .expect("Expected: Rust db")
-            .get_async_connection()
-            .await
-            .expect("Expected: Async connection to Rust db");
-
-        let work_queue = WorkQueue::new(KeyPrefix::from("sanity_custom_sync_rust"));
-
-        loop {
-            let job: Option<Item> = work_queue
-                .lease(db, None, Duration::from_secs(60))
-                .await
-                .expect("Expected: Lease a job");
-
-            match job {
-                Some(job) => {
-                    println!("Processing Thread 2 => Processing Job: {}", job.id);
-                    sleep(PROCESSING_TIME).await;
-                    work_queue
-                        .complete(db, &job)
-                        .await
-                        .expect("Expected: Mark job as complete from processing thread 2.");
-                    println!(
-                        "Processing Thread 2 => Completed Processing Job: {}",
-                        job.id
-                    );
-                }
-                None => {}
-            }
-        }
-    });
-
-    // thread to process jobs from queue
-    tokio::spawn(async {
-        // connect to redis
-        let host = "localhost";
-        let db = &mut redis::Client::open(format!("redis://{host}/"))
-            .expect("Expected: Rust db")
-            .get_async_connection()
-            .await
-            .expect("Expected: Async connection to Rust db");
-
-        let work_queue = WorkQueue::new(KeyPrefix::from("sanity_custom_sync_rust"));
-
-        loop {
-            let job: Option<Item> = work_queue
-                .lease(db, None, Duration::from_secs(60))
-                .await
-                .expect("Expected: Lease a job");
-
-            match job {
-                Some(job) => {
-                    println!("Processing Thread 3 => Processing Job: {}", job.id);
-                    sleep(PROCESSING_TIME).await;
-                    work_queue
-                        .complete(db, &job)
-                        .await
-                        .expect("Expected: Mark job as complete from processing thread 3.");
-                    println!(
-                        "Processing Thread 3 => Completed Processing Job: {}",
-                        job.id
-                    );
-                }
-                None => {}
-            }
-        }
-    });
-
-    // thread to process jobs from queue
-    tokio::spawn(async {
-        // connect to redis
-        let host = "localhost";
-        let db = &mut redis::Client::open(format!("redis://{host}/"))
-            .expect("Expected: Rust db")
-            .get_async_connection()
-            .await
-            .expect("Expected: Async connection to Rust db");
-
-        let work_queue = WorkQueue::new(KeyPrefix::from("sanity_custom_sync_rust"));
-
-        loop {
-            let job: Option<Item> = work_queue
-                .lease(db, None, Duration::from_secs(60))
-                .await
-                .expect("Expected: Lease a job");
-
-            match job {
-                Some(job) => {
-                    println!("Processing Thread 3 => Processing Job: {}", job.id);
-                    sleep(PROCESSING_TIME).await;
-                    work_queue
-                        .complete(db, &job)
-                        .await
-                        .expect("Expected: Mark job as complete from processing thread 3.");
-                    println!(
-                        "Processing Thread 3 => Completed Processing Job: {}",
-                        job.id
-                    );
-                }
-                None => {}
-            }
-        }
-    });
+    // threads to process jobs from queue
+    tokio::spawn(root_route::processing_thread(String::from(
+        "Processing Thread 1",
+    )));
+    tokio::spawn(root_route::processing_thread(String::from(
+        "Processing Thread 2",
+    )));
+    tokio::spawn(root_route::processing_thread(String::from(
+        "Processing Thread 3",
+    )));
 
     // run it with hyper on localhost:4000
     axum::Server::bind(&"0.0.0.0:4000".parse().unwrap())
