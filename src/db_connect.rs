73ece886dca6b01ceba4deaee8799afa06066603
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-10-17 10:03:58 +0600

    refactor: rename file to `db_connect` return redis connection

--- src/db_connect.rs
+++ src/db_connect.rs
@@ -1,24 +1,35 @@
-use std::time::Duration;
-
+use dotenv::dotenv;
 use redis::aio::Connection;
+use std::env;
+use std::time::Duration;
 use tokio::time::sleep;
 
-pub async fn redis_conn_check(redis_url: &str) -> Option<Connection> {
-    // check if we can connect to db.
+/*
+ This function will try to return a redis connection.
+ TODO: We should look into caching the response of this function when
+ we have successfull connection.
+*/
+pub async fn redis_conn() -> Option<Connection> {
+    // Load env form .env
+    dotenv().ok();
+
+    // get redis url from env
+    let redis_url = env::var("REDIS_URL").expect("REDIS_URL is not set in .env");
+
     const RETRY_COUNT: i32 = 10;
     const RETRY_DELAY: Duration = Duration::from_secs(10);
+
     let mut retries = 0;
 
     while retries != RETRY_COUNT {
         println!("Connection Attempt: {retries}");
 
+        // try to connect to redis client
         let client = &mut redis::Client::open(format!("redis://{redis_url}/"));
 
         match client {
             Ok(client) => {
-                /*
-                   try to get a connection
-                */
+                // try to get an async connection from client
                 match client.get_async_connection().await {
                     Ok(conn) => {
                         return Some(conn);
@@ -42,8 +53,6 @@ pub async fn redis_conn_check(redis_url: &str) -> Option<Connection> {
                 retries += 1;
             }
         }
-
-        // let work_queue = WorkQueue::new(KeyPrefix::from("sanity_custom_sync_rust"));
     }
 
     return None;
