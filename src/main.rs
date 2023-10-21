aad7baa3a84ecec6e0181e1f6e94bec69244d13c
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-10-21 16:07:05 +0600

    feat: parse payload

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ mod root_route;
 mod sanity;
 mod shopify_payload;
 
-use axum::{routing::post, Router};
+use axum::{routing::post, Json, Router};
 use serde::Serialize;
+use shopify_payload::RequestPayload;
 use tracing::info;
 use tracing_subscriber;
 
@@ -24,7 +25,10 @@ async fn main() {
     let env_config = env_config::get_env_config();
 
     // build our application with a single route
-    let app = Router::new().route("/", post(root_route::handle));
+    let app = Router::new().route(
+        "/",
+        post(move |Json(payload): Json<RequestPayload>| root_route::handle(payload)),
+    );
 
     // serve it with hyper on designated port
     axum::Server::bind(&format!("0.0.0.0:{}", env_config.port).parse().unwrap())
--- src/root_route.rs
+++ src/root_route.rs
@@ -2,13 +2,17 @@ use axum::{http::StatusCode, Json};
 use serde::Serialize;
 use tracing::info;
 
+use crate::shopify_payload::RequestPayload;
+
 #[derive(Serialize)]
 pub struct Response {
     message: String,
 }
 
-pub async fn handle() -> (StatusCode, Json<Response>) {
+pub async fn handle(payload: RequestPayload) -> (StatusCode, Json<Response>) {
     info!("Route: '/', Ready to accept requests!");
+    println!("{:?}", payload);
+
     (
         StatusCode::OK,
         Json(Response {
