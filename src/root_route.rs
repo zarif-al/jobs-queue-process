fff42d95afad74f631f9f16984c35e4ff72665c4
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-10-23 10:21:56 +0600

    refactor: move image upload process to separate function

--- src/root_route.rs
+++ src/root_route.rs
@@ -1,13 +1,25 @@
 // TODO: Clean up overall code. Look up how to clean up `match` usage
 
 use crate::env_config::get_env_config;
-use crate::sanity::product_schema::SanityProduct;
-use crate::sanity::utils::{get_url, ApiMode};
-use crate::sanity::{Mutations, SanityMutationPayload, SanityResponse};
+use crate::sanity::base::reference::SanityReference;
+use crate::sanity::http_endpoint::{get_sanity_endpoint, ApiMode};
+use crate::sanity::http_payload::{Mutations, SanityMutationPayload};
+use crate::sanity::http_response::{SanityResponse, SanityResponseBody};
+use crate::sanity::schema::product::SanityProduct;
+use crate::sanity::schema::{
+    root::image::{SanityCustomImage, SanityImage},
+    root::link::SanityLink,
+    root::seo::{SanitySEO, SanitySeoOpenGraph, SanitySlug},
+};
+use crate::shopify_payload::product_payload::ShopifyProductImage;
 use crate::shopify_payload::RequestPayload;
-use crate::{client::get_client, db_connect};
+use crate::{
+    db_connect,
+    http_client::{get_http_client, ContenType},
+};
 
 use axum::{extract::Json, http::StatusCode};
+use nanoid::nanoid;
 use redis_work_queue::{Item, WorkQueue};
 use serde::Serialize;
 use std::{sync::Arc, time::Duration};
@@ -95,11 +107,50 @@ pub async fn processing_thread(name: String, work_queue: Arc<WorkQueue>) {
                                 info!("{} => Sync Job Action: {:?}", name, payload.action);
 
                                 for product in payload.products {
+                                    let images: Vec<SanityCustomImage> = upload_image(
+                                        product.images,
+                                        product.title.clone(),
+                                        &name,
+                                        &job.id,
+                                    )
+                                    .await;
+
+                                    let shopify_id: u64 = product
+                                        .id
+                                        .strip_prefix("gid://shopify/Product/")
+                                        .unwrap()
+                                        .parse()
+                                        .unwrap();
+
+                                    let admin_graphql_id: String = product.id;
                                     mutation_payload.mutations.push(Mutations::CreateOrReplace(
                                         SanityProduct {
-                                            title: product.title,
+                                            title: product.title.clone(),
                                             _type: "shopifyProduct".to_string(),
-                                            shopify_id: product.id,
+                                            shopify_id,
+                                            admin_graphql_id,
+                                            seo: SanitySEO {
+                                                title: product.title.clone(),
+                                                description: product.title.clone(),
+                                                slug: SanitySlug {
+                                                    current: product.handle,
+                                                },
+                                                canonicalLink: SanityLink { addLink: false },
+                                                disableFollow: false,
+                                                disableImageIndex: false,
+                                                disableIndex: false,
+                                                disableSnippet: false,
+                                                openGraph: SanitySeoOpenGraph {
+                                                    image: images.first().unwrap().clone(),
+                                                },
+                                            },
+                                            shopify_created_at: product.createdAt,
+                                            shopify_published_at: product.publishedAt,
+                                            shopify_updated_at: product.updatedAt,
+                                            status: product.status,
+                                            tags: product.tags,
+                                            product_type: product.productType,
+                                            images,
                                         },
                                     ))
                                 }
@@ -109,10 +160,10 @@ pub async fn processing_thread(name: String, work_queue: Arc<WorkQueue>) {
                             }
                         }
 
-                        let client = get_client();
+                        let client = get_http_client(ContenType::Data);
 
                         let response = client
-                            .post(get_url(ApiMode::Mutate))
+                            .post(get_sanity_endpoint(ApiMode::Mutate))
                             .bearer_auth(&env_config.sanity_auth_token)
                             .json(&mutation_payload)
                             .send()
@@ -158,37 +209,111 @@ pub async fn processing_thread(name: String, work_queue: Arc<WorkQueue>) {
 // to track how many times a job has been tried.
 //
 // Implementation Two: Push to a mongo db instance.
-async fn handle_failure(name: String, job: Item, work_queue: Arc<WorkQueue>) {
-    // match db_connect::redis_conn().await {
-    //     Some(mut conn) => {
-    //         warn!(
-    //             "{} => Marking job: {} as complete! This is a temporary workaround.",
-    //             name, job.id
-    //         );
+// async fn handle_failure(name: String, job: Item, work_queue: Arc<WorkQueue>) {
+//     // match db_connect::redis_conn().await {
+//     //     Some(mut conn) => {
+//     //         warn!(
+//     //             "{} => Marking job: {} as complete! This is a temporary workaround.",
+//     //             name, job.id
+//     //         );
 
-    //         work_queue
-    //             .complete(&mut conn, &job)
-    //             .await
-    //             .expect("Failed to mark a job as incomplete!");
+//     //         work_queue
+//     //             .complete(&mut conn, &job)
+//     //             .await
+//     //             .expect("Failed to mark a job as incomplete!");
 
-    //         warn!(
-    //             "{} => Adding job: {} to queue! This is a temporary workaround.",
-    //             name, job.id
-    //         );
+//     //         warn!(
+//     //             "{} => Adding job: {} to queue! This is a temporary workaround.",
+//     //             name, job.id
+//     //         );
 
-    //         let item = Item::new(job.data);
+//     //         let item = Item::new(job.data);
 
-    //         work_queue
-    //             .add_item(&mut conn, &item)
-    //             .await
-    //             .expect("Failed to re-add job to queue");
-    //     }
-    //     None => {
-    //         error!(
-    //             "{} => Failed to handle job failure. Job Id: {}",
-    //             name, job.id
-    //         );
-    //     }
-    // }
-    todo!("Code up implementation two");
+//     //         work_queue
+//     //             .add_item(&mut conn, &item)
+//     //             .await
+//     //             .expect("Failed to re-add job to queue");
+//     //     }
+//     //     None => {
+//     //         error!(
+//     //             "{} => Failed to handle job failure. Job Id: {}",
+//     //             name, job.id
+//     //         );
+//     //     }
+//     // }
+//     todo!("Code up implementation two");
+// }
+
+/*
+    This function will upload an array of shopify images to sanity and
+    return a vector of sanity images.
+
+    TODO: There are many points of failures in this function, please address
+    them appropriately.
+*/
+async fn upload_image(
+    shopify_product_images: Vec<ShopifyProductImage>,
+    product_title: String,
+    thread_name: &String,
+    job_id: &String,
+) -> Vec<SanityCustomImage> {
+    let env_config = get_env_config();
+
+    let mut images: Vec<SanityCustomImage> = vec![];
+
+    let client = get_http_client(ContenType::Image);
+
+    info!(
+        "{} => Uploading images for job: {}, Image count: {}",
+        thread_name,
+        job_id,
+        shopify_product_images.len()
+    );
+
+    for image in shopify_product_images {
+        // Download image bytes
+        let featured_image = reqwest::get(image.src).await.unwrap();
+
+        // Push image to sanity dataset.
+        let image_response = client
+            .post(get_sanity_endpoint(ApiMode::Assets))
+            .bearer_auth(&env_config.sanity_auth_token)
+            .body(featured_image)
+            .send()
+            .await
+            .unwrap();
+
+        match image_response.error_for_status_ref() {
+            Ok(_) => {
+                let image = image_response.text().await.unwrap();
+
+                let image_json: SanityResponseBody = serde_json::from_str(&image).unwrap();
+
+                images.push(SanityCustomImage {
+                    alt: Some(product_title.clone()),
+                    imageSrc: SanityImage {
+                        _type: "image".to_string(),
+                        asset: SanityReference {
+                            _ref: image_json.document._id,
+                            _type: "reference".to_string(),
+                        },
+                    },
+                    _key: Some(nanoid!()),
+                })
+            }
+            Err(err) => {
+                let res = image_response.json::<SanityResponse>().await.unwrap();
+
+                error!(
+                    "{} => Failed to upload image. Error: {}. Error Message: {:?}",
+                    thread_name,
+                    err,
+                    res.error.unwrap()
+                );
+                panic!();
+            }
+        }
+    }
+
+    return images;
 }
