335a2bd60002d9e2c0ef505bc223c7f29775a7e1
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-10-24 18:35:23 +0600

    feat: define shopify admin product struct

---
+++ src/shopify_payload/admin_product.rs
@@ -0,0 +1,17 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ShopifyAdminProduct {
+    pub productCategory: ProductCategory,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ProductCategory {
+    pub productTaxonomyNode: ProductTaxonomyNode,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ProductTaxonomyNode {
+    pub id: String,
+    pub name: String,
+}
