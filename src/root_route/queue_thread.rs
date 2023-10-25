64e8a62d865c341e1347744c5a14711fba15d2c1
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-10-25 09:53:00 +0600

    docs: update explanation for queue_thread

--- src/root_route/queue_thread.rs
+++ src/root_route/queue_thread.rs
@@ -9,25 +9,26 @@ use crate::{db_connect, shopify::RequestPayload};
 /*
  This thread will receive jobs from the `/` route handler and
  add it to queue.
+
+ This thread will run in an infinite loop only if a connection to
+ the db is established.
+
+ Otherwise it will panic!().
 */
 pub async fn queue_thread(
     name: String,
     mut rx: Receiver<RequestPayload>,
     work_queue: Arc<WorkQueue>,
 ) {
-    // Get db connection
     match db_connect::redis_conn().await {
         Some(mut conn) => {
             info!("{} => Ready to receive jobs!", name);
 
-            // await to receive jobs from the sender channel in `/`
-            // route handler.
-            // add those jobs to queue.
-            // run in an infinite loop.
             loop {
                 for received in rx.recv().await.iter() {
                     let job = Item::from_json_data(received).unwrap();
 
+                    // add job to queue
                     work_queue
                         .add_item(&mut conn, &job)
                         .await
@@ -37,6 +38,8 @@ pub async fn queue_thread(
                 }
             }
         }
-        None => {}
+        None => {
+            panic!("{} => Failed to connect to db.", name);
+        }
     }
 }
