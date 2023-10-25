b24dda5ef62be65da9f6edd959fb90c52ba59e43
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-10-25 10:49:10 +0600

    refactor: update imports

--- src/root_route/processing_thread.rs
+++ src/root_route/processing_thread.rs
@@ -1,4 +1,5 @@
 mod category;
+mod product_sync;
 mod upload_image;
 
 use std::{sync::Arc, time::Duration};
@@ -12,16 +13,8 @@ use crate::{
     http_client::{get_http_client, ContenType},
     sanity::{
         http_endpoint::{get_sanity_endpoint, ApiMode},
-        http_payload::{Mutations, SanityMutationPayload},
+        http_payload::SanityMutationPayload,
         http_response::SanityResponse,
-        schema::{
-            product::SanityProduct,
-            root::{
-                image::SanityCustomImage,
-                link::SanityLink,
-                seo::{SanitySEO, SanitySeoOpenGraph, SanitySlug},
-            },
-        },
     },
     shopify::RequestPayload,
 };
@@ -61,67 +54,14 @@ pub async fn processing_thread(name: String, work_queue: Arc<WorkQueue>) {
 
                         match job_data {
                             RequestPayload::PayloadProductSync(payload) => {
-                                info!("{} => Sync Job Action: {:?}", name, payload.action);
-
-                                for product in payload.products {
-                                    // separate shopify_id and graphql_id
-                                    let shopify_id: u64 = product
-                                        .id
-                                        .strip_prefix("gid://shopify/Product/")
-                                        .unwrap()
-                                        .parse()
-                                        .unwrap();
-
-                                    let admin_graphql_id: String = product.id;
-
-                                    // get product image references.
-                                    let images: Vec<SanityCustomImage> =
-                                        upload_image::upload_image(
-                                            product.images,
-                                            product.title.clone(),
-                                            &name,
-                                            &job.id,
-                                        )
-                                        .await;
-
-                                    /*
-                                     * TODO: Category Stuff
-                                     */
-
-                                    category::category_check(admin_graphql_id.clone()).await;
-
-                                    mutation_payload.mutations.push(Mutations::CreateOrReplace(
-                                        SanityProduct {
-                                            _id: format!("shopifyProduct-{}", shopify_id),
-                                            title: product.title.clone(),
-                                            _type: "shopifyProduct".to_string(),
-                                            shopify_id,
-                                            admin_graphql_id,
-                                            seo: SanitySEO {
-                                                title: product.title.clone(),
-                                                description: product.title.clone(),
-                                                slug: SanitySlug {
-                                                    current: product.handle,
-                                                },
-                                                canonicalLink: SanityLink { addLink: false },
-                                                disableFollow: false,
-                                                disableImageIndex: false,
-                                                disableIndex: false,
-                                                disableSnippet: false,
-                                                openGraph: SanitySeoOpenGraph {
-                                                    image: images.first().unwrap().clone(),
-                                                },
-                                            },
-                                            shopify_created_at: product.createdAt,
-                                            shopify_published_at: product.publishedAt,
-                                            shopify_updated_at: product.updatedAt,
-                                            status: product.status,
-                                            tags: product.tags,
-                                            product_type: product.productType,
-                                            images,
-                                        },
-                                    ))
-                                }
+                                info!("{} => Commencing Sync Job Action!", name);
+                                product_sync::product_sync(
+                                    &name,
+                                    &job,
+                                    &mut mutation_payload,
+                                    payload,
+                                )
+                                .await;
                             }
                             RequestPayload::PayloadProductDelete(payload) => {
                                 info!("{} => Delete Job Action: {:?}", name, payload.action);
