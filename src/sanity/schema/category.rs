af150ea91e9deb667b9ccc61ef97767687880574
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-10-26 10:11:07 +0600

    refactor: update category definition

--- src/sanity/schema/category.rs
+++ src/sanity/schema/category.rs
@@ -4,16 +4,19 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SanityCategoryCalloutItem {
+    pub _key: String,
     pub title: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SanityCategoryItemProductReference {
+    pub _key: String,
     pub _ref: String,
     pub _type: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
 pub enum SanityCategoryItems {
     SanityProduct(SanityCategoryItemProductReference),
     SanityCategoryCalloutItem(SanityCategoryCalloutItem),
@@ -25,7 +28,8 @@ pub enum SanityCategoryItems {
 */
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SanityCategory {
+    pub r#_type: String,
     pub _id: String,
     pub title: String,
-    pub items: Vec<SanityCategoryItems>,
+    pub items: Option<Vec<SanityCategoryItems>>,
 }
