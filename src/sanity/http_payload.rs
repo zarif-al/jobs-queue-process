cf9913928feb8057a3c737a2ed568020472d8b95
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-10-26 11:38:17 +0600

    feat: create category create and patch mutations

--- src/sanity/http_payload.rs
+++ src/sanity/http_payload.rs
@@ -1,11 +1,32 @@
+#![allow(non_snake_case)]
+
 use serde::Serialize;
 
 use crate::sanity::schema::product::SanityProduct;
+
+use super::schema::category::{SanityCategory, SanityCategoryItems};
+
+#[derive(Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SanityCategoryPatchInsert {
+    pub after: String,
+    pub items: Vec<SanityCategoryItems>,
+}
+
+#[derive(Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SanityCategoryPatch {
+    pub id: String,
+    pub setIfMissing: SanityCategory,
+    pub insert: SanityCategoryPatchInsert,
+}
 
 #[derive(Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub enum Mutations {
     CreateOrReplace(SanityProduct),
+    Create(SanityCategory),
+    Patch(SanityCategoryPatch),
 }
 
 #[derive(Serialize, Debug)]
