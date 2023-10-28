56f4e656f3d17f502cfdff245735862363e7091a
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-10-28 20:12:59 +0600

    refactor: type email as `Email` instead of `String`

--- src/payload.rs
+++ src/payload.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-
+use serde_email::Email;
 #[derive(Serialize)]
 pub struct PostJobResponsePayload {
     pub message: String,
@@ -8,17 +8,17 @@ pub struct PostJobResponsePayload {
 #[derive(Deserialize, Serialize, Debug)]
 pub struct PostJobRequestPayload {
     pub message: String,
-    pub email: String,
+    pub email: Email,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct RequestMessagesPayload {
-    pub email: Option<String>,
+    pub email: Option<Email>,
 }
 
 #[derive(Deserialize, Serialize)]
 pub struct MessagesResponsePayload {
-    pub email: Option<String>,
+    pub email: Option<Email>,
     pub messages: Option<Vec<String>>,
     pub error: Option<String>,
 }
