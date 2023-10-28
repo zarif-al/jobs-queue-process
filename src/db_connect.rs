b63af3a6e3328699ca8c7ff9f16048b295c30dc4
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-10-28 11:09:32 +0600

    refactor: add connection retry attempts for mongo db

--- src/db_connect.rs
+++ src/db_connect.rs
@@ -1,8 +1,15 @@
+use std::time::Duration;
+
 use mongodb::bson::Document;
-use mongodb::error::Error;
 use mongodb::{options::ClientOptions, Client, Collection};
+use tokio::time::sleep;
+use tracing::{error, info};
 
 use crate::env_config::get_env_config;
+
+const RETRY_COUNT: i32 = 10;
+const RETRY_DELAY: Duration = Duration::from_secs(10);
+
 /*
  This function will try to return a redis connection.
  TODO: We should look into caching the response of this function when
@@ -15,25 +22,43 @@ pub async fn redis_conn() {
 /*
  This function will create a connection to mongodb and return it.
 */
-pub async fn mongo_conn() -> Result<Collection<Document>, Error> {
+pub async fn mongo_conn() -> Option<Collection<Document>> {
+    let mut retries = 0;
+
     let env_config = get_env_config();
 
-    let client_options = ClientOptions::parse(env_config.mongo_uri).await;
+    while retries != RETRY_COUNT {
+        if retries > 1 {
+            info!("Mongo Connection Attempt: {retries}");
+        }
 
-    match client_options {
-        Ok(mut options) => {
-            options.app_name = Some("Jobs Queue Process".to_string());
-            let client = Client::with_options(options);
+        let client_options = ClientOptions::parse(env_config.mongo_uri.clone()).await;
 
-            match client {
-                Ok(client) => {
-                    let db = client.database(&env_config.mongo_db_name);
+        match client_options {
+            Ok(mut options) => {
+                options.app_name = Some("Jobs Queue Process".to_string());
+                let client = Client::with_options(options);
 
-                    Ok(db.collection::<Document>("jobs"))
+                match client {
+                    Ok(client) => {
+                        let db = client.database(&env_config.mongo_db_name);
+
+                        return Some(db.collection::<Document>("jobs"));
+                    }
+                    Err(err) => {
+                        error!("Failed to get mongo client. Error: {}", err);
+                        retries += 1;
+                        sleep(RETRY_DELAY).await;
+                    }
                 }
-                Err(err) => Err(err),
+            }
+            Err(err) => {
+                error!("Failed to get mongo client. Error: {}", err);
+                retries += 1;
+                sleep(RETRY_DELAY).await;
             }
         }
-        Err(err) => Err(err),
     }
+
+    None
 }
