75440720016d788369e83c97229b69b25729edc2
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-10-29 00:21:08 +0600

    refactor: rename `payload` to `req_res_structs`

---
+++ src/req_res_structs.rs
@@ -0,0 +1,32 @@
+use serde::{Deserialize, Serialize};
+use serde_email::Email;
+
+#[derive(Serialize, Deserialize)]
+pub struct GeneralResponse {
+    pub message: Option<String>,
+    pub error: Option<String>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct PostJobRequestPayload {
+    pub message: String,
+    pub email: Email,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct MessagesRequestPayload {
+    pub email: Option<Email>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct MessagesResponse {
+    pub email: Email,
+    pub messages: Vec<String>,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum MessagesResponseEnum {
+    MessagesResponse(MessagesResponse),
+    GeneralResponse(GeneralResponse),
+}
