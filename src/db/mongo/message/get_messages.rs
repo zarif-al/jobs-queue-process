78e4690d06f1f9f2a5c29653204e857c8944fe32
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-11-04 17:36:03 +0600

    refactor: move messages fetch logic to `db` -> `mongo` module

---
+++ src/db/mongo/message/get_messages.rs
@@ -0,0 +1,74 @@
+use mongodb::bson::doc;
+use tracing::error;
+
+use crate::db::{mongo_conn, mongo_message::DBMessage};
+
+pub async fn get_messages(email: &String) -> Option<Vec<String>> {
+    let mongo_conn = mongo_conn::<DBMessage>().await;
+
+    match mongo_conn {
+        Some(collection) => {
+            let mongo_query_filter = doc! { "email" : &email };
+
+            // collection.find() returns a cursor that streams the results as it gets iterated
+            let mongo_result_cursor = collection.find(mongo_query_filter, None).await;
+
+            match mongo_result_cursor {
+                Ok(mut cursor) => {
+                    let mut messages: Vec<String> = vec![];
+
+                    loop {
+                        let current_cursor = cursor.advance().await;
+
+                        match current_cursor {
+                            /*
+                               If new results are returned by then attempt to
+                               access data.
+                            */
+                            Ok(doc_exists) => {
+                                if doc_exists {
+                                    match cursor.deserialize_current() {
+                                        Ok(data) => {
+                                            messages.push(data.message);
+                                            continue;
+                                        }
+                                        Err(err) => {
+                                            error!(
+                                                "Failed to get data from cursor. Error Message: {}",
+                                                err
+                                            );
+                                            break;
+                                        }
+                                    }
+                                } else {
+                                    // The cursor is closed. There is no more data.
+                                    break;
+                                }
+                            }
+                            Err(err) => {
+                                error!("Failed to advance cursor. Error Message: {}", err);
+                                break;
+                            }
+                        }
+                    }
+
+                    return Some(messages);
+                }
+                Err(err) => {
+                    error!(
+                        "Failed to get message list for email: {}. Error: {}",
+                        &email, err
+                    );
+                    None
+                }
+            }
+        }
+        None => {
+            error!(
+                "Failed to get message list for {}. Error: Failed to get mongodb connection.",
+                &email
+            );
+            None
+        }
+    }
+}
--- src/graphql/query_root.rs
+++ src/graphql/query_root.rs
@@ -1,9 +1,7 @@
 use async_graphql::{Error, Object};
-use mongodb::bson::doc;
 use serde_email::is_valid_email;
-use tracing::error;
 
-use crate::db::{mongo_conn, mongo_message::DBMessage};
+use crate::db::get_mongo_messages;
 use crate::graphql::helper_structs::ResolvedMessageList;
 
 pub struct GraphQLQueryRoot;
@@ -14,74 +12,13 @@ impl GraphQLQueryRoot {
         let is_valid_email = is_valid_email(&email);
 
         if is_valid_email {
-            let mongo_conn = mongo_conn::<DBMessage>().await;
-
-            match mongo_conn {
-                Some(collection) => {
-                    let mongo_query_filter = doc! { "email" : &email };
-
-                    // collection.find() returns a cursor that streams the results as it gets iterated
-                    let mongo_result_cursor = collection.find(mongo_query_filter, None).await;
-
-                    match mongo_result_cursor {
-                        Ok(mut cursor) => {
-                            let mut messages: Vec<String> = vec![];
-
-                            loop {
-                                let current_cursor = cursor.advance().await;
-
-                                match current_cursor {
-                                    /*
-                                       If new results are returned by then attempt to
-                                       access data.
-                                    */
-                                    Ok(doc_exists) => {
-                                        if doc_exists {
-                                            match cursor.deserialize_current() {
-                                                Ok(data) => {
-                                                    messages.push(data.message);
-                                                    continue;
-                                                }
-                                                Err(err) => {
-                                                    error!(
-                                                "Failed to get data from cursor. Error Message: {}",
-                                                err
-                                            );
-                                                    break;
-                                                }
-                                            }
-                                        } else {
-                                            // The cursor is closed. There is no more data.
-                                            break;
-                                        }
-                                    }
-                                    Err(err) => {
-                                        error!("Failed to advance cursor. Error Message: {}", err);
-                                        break;
-                                    }
-                                }
-                            }
-
-                            return Ok(ResolvedMessageList { messages, email });
-                        }
-                        Err(_) => {
-                            // Return an error response
-                            Err(Error {
-                                message: String::from("Failed to query data from mongo."),
-                                source: None,
-                                extensions: None,
-                            })
-                        }
-                    }
-                }
-                None => {
-                    // Return an error response
-                    Err(Error {
-                        message: String::from("Failed to get mongo connection"),
-                        source: None,
-                        extensions: None,
-                    })
-                }
+            match get_mongo_messages(&email).await {
+                Some(messages) => Ok(ResolvedMessageList { email, messages }),
+                None => Err(Error {
+                    message: String::from("Internal Server Error."),
+                    source: None,
+                    extensions: None,
+                }),
             }
         } else {
             Err(Error {
