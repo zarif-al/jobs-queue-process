cc94e7858a70ea0d7494ba22153db0584f2b334e
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-11-04 15:50:54 +0600

    refactor: remove deleted module

--- src/db.rs
+++ src/db.rs
@@ -3,6 +3,5 @@ mod mongo;
 mod redis;
 
 pub use mongo::connect::mongo_conn;
-pub use mongo::entities as mongo_entities;
 pub use mongo::message as mongo_message;
 pub use redis::connect::redis_conn;
