c72c8bcae40a2392234b9df9a5daabe064cc2951
Abdullah Al Zarif <zarif_al96@outlook.com>
2023-11-04 15:44:59 +0600

    refactor: update exports

--- src/db.rs
+++ src/db.rs
@@ -3,5 +3,6 @@ mod mongo;
 mod redis;
 
 pub use mongo::connect::mongo_conn;
-pub use mongo::message;
+pub use mongo::entities as mongo_entities;
+pub use mongo::message as mongo_message;
 pub use redis::connect::redis_conn;
